1b746536816a8ac683ecfdeab0766c778f63ddac
ssense8sid <96626964+ssense8sid@users.noreply.github.com>
2022-01-26 20:27:13 +0530

    Add files via upload

---
+++ 21104004_assign2.py
@@ -0,0 +1,20 @@
+string1 = "Python is a case sensitive language"
+
+#option a
+print (len(string1)) #inbuilt function to find length
+
+#option b
+print (string1[::-1])
+
+#option c
+string2 = string1[10:27]
+print (string2)
+
+#option d
+print (string1.replace("a case sensitive","object oriented"))
+
+#option e
+print (string1.find("a"))
+
+#option f
+print (string1.replace(" ",""))
